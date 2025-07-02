614ed535dbc20bf532939aaff1764c9e51996d71
aravindsgit9298 <aravind.suresh.qe@gmail.com>
2025-07-02 11:36:30 +0530

    assignment update

--- src/assignments/Employees.java
+++ src/assignments/Employees.java
@@ -1,11 +1,14 @@
 package assignments;
 
 public class Employees {
+
 	
+	//Employee Names
 	String empName1 = "Bharath";
 	String empName2 = "ABC";
 	String empName3 = "XYZ";
-	
+
+	//Employee IDs
 	int emp1Id = 1234;
 	int emp2Id = 1235;
 	int emp3Id = 1236;
